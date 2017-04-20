8e4066389297100e828632eedfc18070ffdee009
Erick Garcia <egarcia@wkm.com.br>
2017-04-20 18:37:42 -0300

    Fixed references and depencies to net framework

--- samples/AspNetSample/Program.cs
+++ samples/AspNetSample/Program.cs
@@ -11,7 +11,6 @@ namespace AspNetSample
 				.UseKestrel()
 				.UseContentRoot(Directory.GetCurrentDirectory())
 				.UseUrls("http://localhost:60000", "http://localhost:60001", "http://localhost:60002", "http://localhost:60003")
-				.UseIISIntegration()
 				.UseStartup<Startup>()
 				.Build();
 
